7aa01d14ccc13f15a382513ec5b0a7c070c1e3c6
Ronald George <ronaldsamgeorge@gmail.com>
2022-12-06 00:46:34 +0530

    Refactor index to only start server and move functionality to App

---
+++ app.js
@@ -0,0 +1,26 @@
+const logger = require('./utils/logger');
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const blogRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
+const cors = require('cors');
+const config = require('./utils/config');
+
+const mongoURL = config.mongo_url;
+
+mongoose.connect(mongoURL)
+.then(() => {
+    logger.info('connected to Database');
+})
+.catch(err => logger.error(err));
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/blogs', blogRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,32 +1,11 @@
 const config = require("./utils/config");
 const logger = require("./utils/logger")
 const http = require('http');
-const express = require('express');
-const cors = require('cors');
-const { default: mongoose } = require("mongoose");
-const app = express();
-const blogRouter = require('./controllers/blogs');
-const middleware = require('./utils/middleware');
-const { nextTick } = require("process");
+const app = require('./app');
 
-const mongoURL = config.mongo_url;
-
-app.use(cors());
-app.use(express.json());
-
-mongoose.connect(mongoURL)
-.then(() => {
-    logger.info('connected to Database');
-})
-.catch(err => logger.error(err));
-
-
-app.use('/api/blogs', blogRouter);
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
-
-
+const server = http.createServer(app);
 const PORT = config.port || 8080;
-app.listen(PORT, () => {
+
+server.listen(PORT, () => {
     logger.info(`Listening on port : ${PORT}`);
 })
